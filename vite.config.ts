d3c8452b74a998f3a96ded0154366c269bf48de6
oscody <bogleoneil@gmail.com>
2025-10-06 23:36:32 -0400

    Add homepage URL to package.json, update Vite config for dynamic base URL, and copy .nojekyll during deployment

--- vite.config.ts
+++ vite.config.ts
@@ -22,5 +22,7 @@ export default defineConfig({
     outDir: "dist",
     emptyOutDir: true,
   },
-  base: process.env.NODE_ENV === "production" ? "/ShopifyStore_Client/" : "/",
+  base:
+    process.env.VITE_BASE_URL ||
+    (process.env.NODE_ENV === "production" ? "/ShopifyStore_Client/" : "/"),
 });
